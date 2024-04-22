490988c37605a290f632b59d3e2878ce7bcb52f9
SergioTanakaAtos <105051554+SerginhoTanaka@users.noreply.github.com>
2024-04-22 11:39:10 -0300

    Initial page

--- escalation/urls.py
+++ escalation/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.initial_page, name='initial_page')
+    path('', views.initial_page, name='initial_page'),
+    path('escalation/group/<int:group_id>/<int:user_id>/', views.escalation, name='escalation'),
 ]
--- escalation/views.py
+++ escalation/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from .models import Escalation
+from .models import UserGroupDefault
 
+from django.http import HttpResponseForbidden
 # Create your views here.
 def initial_page(request):
-    return render(request, 'escalation/initial_page.html')
+    groups = Group.objects.all()
+    return render(request, 'escalation/initial_page.html', {'groups': groups})
+
+def escalation(request, group_id, user_id):
+    group = Group.objects.get(id=group_id)
+    user = User.objects.get(id=user_id)
+    
+    if not UserGroupDefault.objects.filter(user_id=user, group_id=group, is_visualizer=True).exists():
+        return HttpResponseForbidden("Você não tem permissão para visualizar este cliente.")
+
+    escalation = Escalation.objects.filter(group=group)
+    
+    return render(request, 'escalation/group_page.html', {'group': group, 'escalation': escalation})
