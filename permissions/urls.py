7d27ed83f27d4d9d7805c65d89d79a74dbf2e33b
melissaSBNeves <145376881+MelissaSNeves@users.noreply.github.com>
2024-04-25 14:26:18 -0300

    update permissions

--- permissions/urls.py
+++ permissions/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.initial_page, name='initial_page'),
-    path('escalation/<int:group_id>/<int:user_id>/', views.escalation, name='escalation'),
+    path('', views.permissions, name='permissions'),
 ]
--- permissions/views.py
+++ permissions/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def permissions(request):
+    return render(request, 'permission.html')
