0a534aeffd067b688fd805645aada4958fa548ec
SergioTanakaAtos <105051554+SerginhoTanaka@users.noreply.github.com>
2024-04-26 14:45:11 -0300

    Adicionando cliente

--- escalation/urls.py
+++ escalation/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.initial_page, name='initial_page'),
+    path('save_group/', views.save_group, name='save_group'),
     path('escalation/<int:group_id>/<int:user_id>/', views.escalation, name='escalation'),
     path('create_escalation/<int:group_id>', views.create_escalation, name='create_escalation'),
 ]
