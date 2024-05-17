78dc3f7710a2d1488d06c0335be7b0fa86f269a6
melissaSBNeves <145376881+MelissaSNeves@users.noreply.github.com>
2024-05-17 15:17:24 -0300

    Update management

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from escalation.models import LogPermission
 
 # Create your views here.
 def index(request):
@@ -9,8 +10,27 @@ def register(request):
     return render(request, 'users/cadastro.html')
 
 def get_users(request):
-    user = User.objects.all()
-    return render(request, 'users/management.html', {'users': user})
+    users = User.objects.all()
+
+    def get_groups(user_id):
+        # Obtem os IDs dos grupos associados ao usuário
+        groups_ids = LogPermission.objects.filter(user_id=user_id).values_list('group_id', flat=True)
+        # Obtem os grupos baseados nos IDs
+        groups = Group.objects.filter(id__in=groups_ids)
+        return groups
+
+
+    users_for_management = []
+    for user in users:
+        user_data = {
+            'name': user.username,
+            'groups': get_groups(user.id)
+        }
+        users_for_management.append(user_data)
+    
+    return render(request, 'users/management.html', {'users': users_for_management})
+
+
 
 def cadastro_exemplo(request):
     user = User.objects.create_user("teste", "lennon@thebeatles.com", "johnpassword")
