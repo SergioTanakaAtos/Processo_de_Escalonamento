4161072a443d260be3db8def5f6a05395d20526a
SergioTanakaAtos <105051554+SerginhoTanaka@users.noreply.github.com>
2024-05-23 15:31:13 -0300

    upload xlsx

--- escalation/views.py
+++ escalation/views.py
@@ -17,6 +17,15 @@ from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
 import pandas as pd
 # Create your views here.
+
+from collections import defaultdict
+from django.shortcuts import render
+from .models import Escalation
+from anytree import Node, RenderTree
+from anytree.exporter import DotExporter
+import graphviz
+from collections import defaultdict
+
 def initial_page(request):
     #pylint: disable=E1101
     groups = Group.objects.all()
@@ -62,43 +71,37 @@ def edit_group(request):
         return JsonResponse({'message':'Grupo criado com sucesso.'}, status=200)
     
     return render(request, 'initial_page.html')     
+
 @csrf_exempt
 @user_passes_test(lambda u: u.is_superuser or u.is_staff)
 def load_data(request):
-    if request.method == 'POST' and request.FILES['csv_file']:
-        csv_file = request.FILES['csv_file']
+    if request.method == 'POST' and request.FILES['xlsx_file']:
+        xlsx_file = request.FILES['xlsx_file']
         
-        upload_dir = os.path.join(settings.BASE_DIR, 'escalation/static/data')
+        upload_dir = os.path.join(settings.BASE_DIR, 'escalation/data')
                 
         os.makedirs(upload_dir, exist_ok=True)
         
-        # Save the file
         fs = FileSystemStorage(location=upload_dir)
-        filename = fs.save(csv_file.name, csv_file)
+        filename = fs.save(xlsx_file.name, xlsx_file)
         
         file_path = fs.path(filename)
-        df = pd.read_csv(file_path)
-        df_group = df['group']
-        if Group.objects.filter(name__in=df_group).exists():
-            return JsonResponse({'message':'O nome do grupo já existe.'}, status=400)
+        df = pd.read_excel(file_path,engine='openpyxl')
         for index, row in df.iterrows():
-            group_name = row['group']
-            group = Group.objects.get(name=group_name)
-
-            name = row['nome']
-            position = row['position']
-            phone = row['phone']
-            email = row['email']
-            level = row['level']
-            area = row['area']
-            service = row['service']
-            if Escalation.objects.filter(group=group, name=name).exists():
-                return JsonResponse({'message':'Já existe um escalonamento com este nome.'}, status=400)
-            escalation = Escalation(name=name, position=position, phone=phone, email=email, level=level, area=area, service=service, group=group)
-            escalation.save()
-        
-        return JsonResponse({'message':'Grupos e escalations criados com sucesso.'}, status=200)
-    
+            group_name = row['Empresa'].replace(' ', '')
+            group, created = Group.objects.get_or_create(name=group_name)
+            name = row['Nome'].replace(' ', '')
+            position = row['Cargo']
+            phone = row['Telefone'] if not pd.isnull(row['Telefone']) else ''
+            email = row['Email']
+            level = row['Nível']
+            area = row['Área']
+            service = row['Serviço']
+            #pylint: disable=E1101
+            if not Escalation.objects.filter(name=name, group=group).exists():
+                escalation = Escalation(name=name, position=position, phone=phone, email=email, level=level, area=area, service=service, group=group)
+                escalation.save()
+        return redirect('initial_page')    
 def escalation(request, group_id, user_id):
     #pylint: disable=E1101
     group = Group.objects.get(id=group_id)
@@ -111,7 +114,7 @@ def escalation(request, group_id, user_id):
     
 
     escalation = Escalation.objects.filter(group=group)
-    
+    escalation_tree()
     if not escalation:
         return render(request, 'escalation/escalation_page.html', {'group': group, 'message': "Não há escalonamento cadastrado para este grupo."})
     return render(request, 'escalation/escalation_page.html', {'group': group, 'escalation': escalation})
@@ -139,3 +142,55 @@ def create_escalation(request, group_id):
         
             return render(request, 'escalation/create_escalation.html', {'group': group})
     return render(request, 'escalation/create_escalation.html', {'group': group})
+
+
+
+def escalation_tree():
+    # pylint: disable=E1101\w
+    escalation = Escalation.objects.order_by('level')
+    tree = defaultdict(list)
+    for esc in escalation:
+        tree[esc.level].append(esc.name)
+
+    # Convertendo o defaultdict para um dicionário normal
+    tree = dict(tree)
+    print(tree)
+
+    # Construindo a árvore com anytree
+    root = Node("Escalation")
+
+    # Dicionário para armazenar os nós por nível
+    nodes = {0: root}
+    for level in sorted(tree.keys()):
+        for name in tree[level]:
+            # Cada nó é filho do nível anterior
+            parent = nodes[level - 1]
+            nodes[level] = Node(name, parent=parent)
+
+    # Exportando a árvore para um arquivo DOT
+    DotExporter(root).to_dotfile("tree.dot")
+
+    # Convertendo o arquivo DOT para um gráfico SVG usando graphviz
+    with open("tree.dot") as f:
+        dot_graph = f.read()
+    graph = graphviz.Source(dot_graph)
+    graph.render("tree", format="svg", cleanup=True)
+
+    # Gerar o conteúdo HTML para visualizar a árvore
+    html_content = """
+
+        <h1>Escalation Tree Visualization</h1>
+        <img src="/static/tree.svg" alt="Tree Visualization">
+    </body>
+    </html>
+    """
+
+    # Salvando o HTML em um arquivo
+    with open("templates/tree.html", "w") as file:
+        file.write(html_content)
+
+    return "tree.html"
+
+def tree_view(request):
+    html_file = escalation_tree()
+    return render(request, html_file)
