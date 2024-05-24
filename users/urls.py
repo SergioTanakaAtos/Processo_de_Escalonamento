a1c35aee9c54de7b6f8d39ad480ede7e4250fffe
SergioTanakaAtos <125500468+SergioTanakaAtos@users.noreply.github.com>
2024-05-24 11:11:58 -0300

    update

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path 
+from django.urls import path
 from . import views
-
+ 
 urlpatterns = [
-    path('', views.index, name='index'),
+
+    path('register/', views.register, name='register'),
+    path('', views.login, name='login'),
+    path('get_groups/', views.get_user_groups, name='user_groups'),
     path('management/', views.get_users, name='management'),
-    path('register/', views.register, name="register"),
-    path('cadastro-exemplo/', views.cadastro_exemplo, name='cadastro-exemplo'),
-    path('get_groups/', views.get_user_groups, name='user_groups')
 ]
+ 
--- users/views.py
+++ users/views.py
@@ -1,14 +1,47 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.http import JsonResponse
-from django.contrib.auth.models import User, Group
 from escalation.models import LogPermission
+from django.contrib.auth.models import Group
 
-# Create your views here.
-def index(request):
-    return render(request, 'users/index.html')
-
+ 
 def register(request):
-    return render(request, 'users/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        permissoes = request.POST.getlist('permissao')
+        confirmar_senha = request.POST.get('confirmarSenha')
+ 
+        if senha != confirmar_senha:
+            return render(request, 'users/register.html', {'error': 'As senhas não coincidem!'})
+ 
+        if not email.endswith('@atos.net'):
+           return render(request, 'users/register.html', {'error': 'O e-mail fornecido não é válido'})
+        user = User.objects.create_user(usuario, email, senha)
+        user.save()
+        return render(request, 'users/login.html')
+    
+    return render(request, 'users/register.html')
+ 
+def login(request):
+    if request.method == 'POST':
+        email = request.POST["email"]
+        password = request.POST["senha"]
+        user = authenticate(request, email=email, password=password)
+        print(f"user = {user}, email = {email}, senha = {password}")
+        if user is not None:
+           # login(request, user)
+            return redirect('initial_page')
+        return render(request, 'users/login.html', {'error': 'Usuário ou senha inválidos'})
+    return render(request, 'users/login.html')
+ 
+def logout(request):
+    logout(request)
+    return redirect('')
 
 def get_users(request):
     users = User.objects.all()
@@ -19,10 +52,10 @@ def get_users(request):
             'name': user.username,
         }
         users_for_management.append(user_data)
-    
+   
     return render(request, 'users/management.html', {'users': users_for_management})
-
-
+ 
+ 
 def get_user_groups(request):
     if request.method == "GET":
         id = int(request.GET.get('id'))
@@ -30,10 +63,3 @@ def get_user_groups(request):
         groups = Group.objects.filter(id__in=groups_ids)
         data = list(groups.values('id', 'name'))
         return JsonResponse({'groups': data})
-
-
-def cadastro_exemplo(request):
-    user = User.objects.create_user("teste", "lennon@thebeatles.com", "johnpassword")
-    user.set_username = "Lennon"
-    user.save()
-    return HttpResponse("Usuário salvo com sucesso!")
