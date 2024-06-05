2f8c9c08708d9d60214d3f7708d0799daf2950da
SergioTanakaAtos <125500468+SergioTanakaAtos@users.noreply.github.com>
2024-06-05 11:55:17 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,6 +29,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
 # Application definition
 
 INSTALLED_APPS = [
