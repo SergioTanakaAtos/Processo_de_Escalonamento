4b9e04a637e81df0d589998c888b96c7a70d34cb
SergioTanakaAtos <125500468+SergioTanakaAtos@users.noreply.github.com>
2024-07-02 11:25:22 -0300

    permissions unitest

--- permissions/urls.py
+++ permissions/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.permissions, name='permissions'),
-    path('create/<int:group_id>', views.save_permission, name='create-permission'),
-    path('<str:action>/<int:permission_id>', views.action_permission, name='action-permission'),
+    path('create/<int:group_id>/', views.save_permission, name='create-permission'),
+    path('<str:action>/<int:permission_id>/', views.action_permission, name='action-permission'),
 ]
